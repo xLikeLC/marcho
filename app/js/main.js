6da1d6fb5d98f6d0d2838533fee9d67a3a845ad5
Constantin <lcmanagernext@gmail.com>
2023-06-04 14:17:06 +0300

    added top-slider section

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,9 @@
-(function () {});
+$(function () {
+  $(".top-slider__inner").slick({
+    dots: true,
+    arrows: false,
+    fade:true;
+    autoplay:true,
+    autoplaySpead: 2000
+  });
+});
--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,11 @@ function styles() {
 }
 
 function scripts() {
-  return src(["node_modules/jquery/dist/jquery.js", "app/js/main.js"])
+  return src([
+    "node_modules/jquery/dist/jquery.js",
+    "node_modules/slick-carousel/slick/slick.js",
+    "app/js/main.js",
+  ])
     .pipe(concat("main.min.js"))
     .pipe(uglify())
     .pipe(dest("app/js"))
